790b7429ff4f59cbdc363f0b8630dcab46c4573b
Kahngjoon Koh <kahngjoonk@gmail.com>
2025-04-03 19:57:29 +0900

    chore - created dev release and removed redundant files

---
+++ README.md
@@ -0,0 +1,9 @@
+# PyBithumb2
+Bithumb API 2.0 wrapper for Python environments using Pydantic models and the python typing library to enhance developer experience.
+
+## Installation
+`pip install pybithumb2`
+
+## Credits
+Alpaca-trading python SDK
+Python-KIS
--- pybithumb2/__env__.py
+++ pybithumb2/__env__.py
@@ -2,8 +2,7 @@ import sys
 
 API_BASE_URL = "https://api.bithumb.com"
 
-VERSION = "{{VERSION_PLACEHOLDER}}"  # This is automatically set via a tag in GitHub Workflow.
-VERSION = "25+dev" if "VERSION_PLACEHOLDER" in VERSION else VERSION
+VERSION = "0.0.1.dev0"
 
 USER_AGENT = f"pybithumb2/{VERSION}"
 
@@ -14,5 +13,5 @@ __author_email__ = "kahngjoonk@gmail.com"
 __url__ = "https://github.com/kahngjoonkoh/pybithumb2"
 __license__ = "MIT"
 
-# if sys.version_info < (3, 10):
-#     raise RuntimeError(f"Pybithumb2 requires Python 3.10+ (Current: {sys.version})")
+if sys.version_info < (3, 10):
+    raise RuntimeError(f"Pybithumb2 requires Python 3.10+ (Current: {sys.version})")
